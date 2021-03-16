df084e0d90818774ca86e25e2d246f08ea3e8a2f
Uduru <kurasiki.homura@gmail.com>
2021-03-16 14:56:26 +0800

    Rewrite main page with display:grid and other fixes

---
+++ Requirements/self-mesured-layout.md
@@ -0,0 +1,15 @@
+> This file contains approximate layout mesurements since the requirements lacks many info.
+> 
+
+# Main Calender Page
+- Header: shorter than calender part
+- Header underline width: 25% of viewport 
+- Header font: Candara (Identical to the example image)
+- Horizontal width of calender: 70% of viewport
+- Blocksize of each month: 284x310
+- Gap between months: 2% of view port
+
+
+## Unsure & Gusses
+- Min width for calender: ?
+- Letter gap: ?
--- date_gen.js
+++ date_gen.js
@@ -1,45 +1,64 @@
-var i = 0, j = 0;  /* temp variable */
+/* Table for 2021 */
+const day_of_month = [-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+/* SUN MON TUE WED THU FRI SAT */
+const starting_weekday = [-1, 5, 1, 1, 4, 6, 2, 4, 0, 3, 5, 1, 3];
+const holiday = [[1, 1], [2, 10], [2, 11], [2, 12], [2, 15], 
+                 [2, 16], [3, 1], [4, 2], [4, 5], [6, 14],
+                 [9, 20], [9, 21], [10, 11], [12, 31]];
+const weekdays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+const month_name = [-1, "JAN", "FEB", "MAR", "APR", "MAY", "JUN", 
+                        "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+const year = 2021;  /* Unused */
 
+function date_gen(month){
+    let i = 0, j = 0;
 
-/* Weekday headers */
-for(i = 0; i < 7; ++i){
-    document.writeln("<span class=\"day weekday-header\">" + weekdays[i] + "</span>");
-}
+    /* Month container*/
+    document.writeln("<div class=\"month\">");
+    document.writeln("<div class=\"month-title text-center\">" + month_name[month] + "</div>");
 
-/* blanks */
-for(i = 0; i < starting_weekday[month]; ++i){
-    document.writeln("<span class=\"day emptyday\"></span>");
-}
+    /* Weekday headers */
+    for(i = 0; i < 7; ++i){
+        document.writeln("<span class=\"weekday-header text-center\">" + weekdays[i] + "</span>");
+    }
 
-/* Fill dates */
-for(i = 1; i <= day_of_month[month]; ++i){
-    var is_holiday = false, weekday = (i + starting_weekday[month]) % 7;
-    for(j = 0; j < holiday.length; ++j){
-        if(month + 1 == holiday[j][0] && i == holiday[j][1]){
+    /* blanks */
+    for(i = 0; i < starting_weekday[month]; ++i){
+        document.writeln("<span class=\"day emptyday\"></span>");
+    }
+
+    /* Fill dates */
+    for(i = 1; i <= day_of_month[month]; ++i){
+        var is_holiday = false, weekday = (i + starting_weekday[month]) % 7;
+        for(j = 0; j < holiday.length; ++j){
+            if(month == holiday[j][0] && i == holiday[j][1]){
+                is_holiday = true;
+                break;
+            }
+        }
+
+        console.log(weekday);
+
+        if(weekday == 0 || weekday == 1){
             is_holiday = true;
-            break;
+        } 
+
+        if(is_holiday){
+            document.writeln("<span class=\"day holiday\">" + i + "</span>");
+        }
+        else{
+            document.writeln("<span class=\"day workday\">" + i + "</span>");
         }
     }
 
-    console.log(weekday);
-
-    if(weekday == 0 || weekday == 1){
-        is_holiday = true;
-    } 
-
-    if(is_holiday){
-        document.writeln("<span class=\"day holiday\">" + i + "</span>");
+    /* Fill blanks until last slot */
+    j = i + starting_weekday[month];
+    while(j % 7 != 1){
+        document.writeln("<span class=\"day emptyday\"></span>");
+        ++j;
     }
-    else{
-        document.writeln("<span class=\"day workday\">" + i + "</span>");
-    }
-}
 
-/* Fill blanks until last slot */
-j = i + starting_weekday[month];
-while(j % 7 != 1){
-    document.writeln("<span class=\"day emptyday\"></span>");
-    ++j;
-}
+    document.writeln("</div>");
 
-++month;  /* Increment for next generation */
+    return;
+}
